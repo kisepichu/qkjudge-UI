bfed3fd904d14eb96d27247a928b941cb0f97f84
tqkoh <shktmygs@gmail.com>
2022-09-08 20:02:50 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), WindiCSS()]
+  plugins: [react(), WindiCSS()],
+  base: '/qkjudge/'
 })
