c3647ea88e8d3ffeeacc4f274aeb4c2399b3f7d4
tqkoh <shktmygs@gmail.com>
2022-09-08 19:50:39 +0900

    deploy test
    qkjudge リポジトリの gh-pages に直接入れる

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,8 @@
 cd dist
 git init
 git checkout -b master
-git add -A
+git add .
 git commit -m 'deploy'
-git push -f git@github.com:tqkoh/qkjudge-UI.git master:dist
+git push -f git@github.com:tqkoh/qkjudge.git master:gh-pages
 rm -rf .git
-cd ..
-
-cd ../tqk.blue
-git pull
-git commit -m "deploy qkjudge" --allow-empty
-git push
 cd ..
